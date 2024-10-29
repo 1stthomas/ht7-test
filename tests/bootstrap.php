14ac2f18dd60fcb89cff4f93c3f31c53a2d759a3
1stthomas <thomas.pluess@students.ffhs.ch>
2024-10-29 21:50:01 +0100

    #33 fix composer dependencies; remove useless comments

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,6 @@
 <?php
-
-/**
- * @author zoom4u
- */
 // TODO: check include path
 //ini_set('include_path', ini_get('include_path'));
-// put your code here
 
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', '1');
