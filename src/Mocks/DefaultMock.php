59174cbd210fd9ef260412b82b12fc87e91d3a94
1stthomas <thomas.pluess@students.ffhs.ch>
2024-11-12 18:42:53 +0100

    Fix first PHPStan findings

--- src/Mocks/DefaultMock.php
+++ src/Mocks/DefaultMock.php
@@ -7,7 +7,17 @@ use PHPUnit\Framework\MockObject\MockObject;
 
 class DefaultMock extends TestCase
 {
+    /**
+     * @psalm-template RealInstanceType of object
+     * @psalm-param class-string<RealInstanceType> $className
+     */
     public function __construct(private string $className) {}
+    /**
+     * @psalm-param list<non-empty-string> $methods
+     * @param   array<int, string>   $methods            The methods to mock.
+     * @param   array<string, mixed>   $constructorArgs    The arguments for the constructor.
+     * @return  MockObject
+     */
     public function create(array $methods = [], ?array $constructorArgs = null): MockObject
     {
         $mb = $this->getMockBuilder($this->className)
