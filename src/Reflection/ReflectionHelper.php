3e2b77215ead385ec4f12ce8cc667c04415df180
1stthomas <thomas.pluess@students.ffhs.ch>
2024-11-22 22:18:20 +0100

    fix php-cs-fixer

--- src/Reflection/ReflectionHelper.php
+++ src/Reflection/ReflectionHelper.php
@@ -1,21 +1,26 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Ht7\Test\Reflection;
 
 class ReflectionHelper
 {
     /**
      * @template T of object
+     *
      * @var \ReflectionClass<object>
      */
     public $reflectedClass;
+
     /**
      * @psalm-param class-string<object>|\ReflectionClass<object> $class
      */
-    public function __construct(string|\ReflectionClass $class)
+    public function __construct(\ReflectionClass|string $class)
     {
         $this->reflectedClass = is_string($class) ? new \ReflectionClass($class) : $class;
     }
+
     public function getConstructor(?bool $isAccessable = true): ?\ReflectionMethod
     {
         $construct = $this->reflectedClass->getConstructor();
@@ -24,6 +29,7 @@ class ReflectionHelper
 
         return $construct;
     }
+
     public function getMethod(string $name, ?bool $isAccessable = true): \ReflectionMethod
     {
         /** @var \ReflectionMethod $method */
@@ -32,6 +38,7 @@ class ReflectionHelper
 
         return $method;
     }
+
     public function getProperty(string $name, ?bool $isAccessable = true): \ReflectionProperty
     {
         $property = $this->reflectedClass->getProperty($name);
