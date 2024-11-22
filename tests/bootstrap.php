830460910158c06ae4b23d4265af816e01f1e0f9
1stthomas <thomas.pluess@students.ffhs.ch>
2024-11-22 18:33:06 +0100

    fixes from php-cs-fixer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
-// TODO: check include path
-//ini_set('include_path', ini_get('include_path'));
+
+// ini_set('include_path', ini_get('include_path'));
 
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 
-$loader = require __DIR__ . '/../vendor/autoload.php';
+$loader = require __DIR__.'/../vendor/autoload.php';
