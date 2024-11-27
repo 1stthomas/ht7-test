a55346348432887e078453bc0c87fa2c87573520
1stthomas <thomas.pluess@students.ffhs.ch>
2024-11-27 18:56:51 +0100

    #15 Fix code coverage

--- tests/Unit/Mocks/DefaultMockTest.php
+++ tests/Unit/Mocks/DefaultMockTest.php
@@ -21,6 +21,9 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(DefaultMock::class)]
 final class DefaultMockTest extends TestCase
 {
+    /** @psalm-var class-string<object> */
+    private string $className = DefaultMock::class;
+
     #[Test]
     #[TestDox('Create a default mock with triggering the constructor.')]
     public function createWithConstruct(): void
@@ -56,6 +59,31 @@ final class DefaultMockTest extends TestCase
 
         /** @var MockObject&TestHelperWithoutConstruct $mock */
         $this->assertSame($expected, $mock->getTest1($msg));
+    }
+
+    #[Test]
+    #[TestDox('Set the class name.')]
+    /**
+     * @covers \Ht7\Test\Mocks\DefaultMock::setClassName
+     */
+    public function setClassName(): void
+    {
+        $sut = $this->getMockBuilder($this->className)
+            ->disableOriginalConstructor()
+            ->onlyMethods([])
+            ->getMock();
+
+        /** @var DefaultMock<RealInstanceType>&MockObject $sut */
+        $return = $sut->setClassName($this->className);
+
+        $this->assertSame($sut, $return);
+        $getClassName = fn(): string => $this->className;
+        // @phpstan-ignore method.resultUnused
+        $getClassName->bindTo($sut, $sut);
+        $name = $getClassName();
+        // $isNew = $getClassName->bindTo($sut, $sut);
+        // unset($isNew);
+        $this->assertSame($this->className, $name);
     }
 }
 
