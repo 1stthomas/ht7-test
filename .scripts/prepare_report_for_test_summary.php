320d2e21494661d76acd6f0ae3278080c36aac8a
1stthomas <thomas.pluess@students.ffhs.ch>
2024-12-13 22:33:13 +0100

    #15 add script to fix test report

---
+++ .scripts/prepare_report_for_test_summary.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+echo getcwd() . PHP_EOL;
+$dom = new DOMDocument();
+$dom->loadXML(file_get_contents('../phpunit-report.xml'));
+
+$testsuites = $dom->getElementsByTagName('testsuites')->item(0);
+$testsuiteTotal = $testsuites->firstChild;
+$testsuiteUnit = $testsuiteTotal->firstChild;
+$testsuitesSearched = $testsuiteUnit->childNodes;
+
+$testsuites->replaceChildren($testsuitesSearched);
+$dom->save('../phpunit-report.xml');
