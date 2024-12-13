0f26fd143f008db9a932e1a735bbc9d916462cdc
1stthomas <thomas.pluess@students.ffhs.ch>
2024-12-14 00:22:16 +0100

    #15 fix path to test report

--- .scripts/prepare_report_for_test_summary.php
+++ .scripts/prepare_report_for_test_summary.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 echo getcwd() . PHP_EOL;
 $dom = new DOMDocument();
-$dom->loadXML(file_get_contents('../phpunit-report.xml'));
+$dom->loadXML(file_get_contents('./phpunit-report.xml'));
 
 $testsuites = $dom->getElementsByTagName('testsuites')->item(0);
 $testsuiteTotal = $testsuites->firstChild;
@@ -12,4 +12,4 @@ $testsuiteUnit = $testsuiteTotal->firstChild;
 $testsuitesSearched = $testsuiteUnit->childNodes;
 
 $testsuites->replaceChildren($testsuitesSearched);
-$dom->save('../phpunit-report.xml');
+$dom->save('./phpunit-report.xml');
