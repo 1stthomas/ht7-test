e75e241bc4effefadb98e41d8fb92dd88ae974eb
1stthomas <thomas.pluess@students.ffhs.ch>
2024-12-14 01:10:46 +0100

    #15 Try to fix dom parsing, add debug info

--- .scripts/prepare_report_for_test_summary.php
+++ .scripts/prepare_report_for_test_summary.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 echo getcwd() . PHP_EOL;
 $dom = new DOMDocument();
 $dom->loadXML(file_get_contents('./phpunit-report.xml'));
+echo '-------------------' . PHP_EOL;
+echo $dom->saveXML();
+echo '-------------------' . PHP_EOL;
+
 
 $testsuites = $dom->getElementsByTagName('testsuites')->item(0);
 $testsuiteTotal = $testsuites->firstChild;
@@ -12,9 +16,10 @@ $testsuiteTotal = $testsuites->firstChild;
 // $testsuitesSearched = $testsuiteUnit->childNodes;
 echo 'Length: ' . $testsuiteTotal->childNodes->length . PHP_EOL;
 $testsuiteSearched = $testsuiteTotal->firstChild;
+echo 'tag name: ' . $testsuiteSearched->nodeName . PHP_EOL;
 // $testsuitesSearched = $testsuiteTotal->childNodes;
 
 
 // $testsuites->replaceChildren($testsuitesSearched);
-$testsuites->replaceChild($testsuiteSearched, $testsuiteTotal);
+// $testsuites->replaceChild($testsuiteSearched, $testsuiteTotal);
 $dom->save('./phpunit-report.xml');
