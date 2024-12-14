75b59a3fdd283c5be25a9e5dc3a401c89cc840d8
1stthomas <thomas.pluess@students.ffhs.ch>
2024-12-14 01:53:40 +0100

    #15 Try to fix dom parsing, add debug info, avoid running full unit test matrix

--- .scripts/prepare_report_for_test_summary.php
+++ .scripts/prepare_report_for_test_summary.php
@@ -11,16 +11,42 @@ echo '-------------------' . PHP_EOL;
 
 
 $testsuites = $dom->getElementsByTagName('testsuites')->item(0);
-/** @var DOMElement $testsuiteTotal */
-$testsuiteTotal = $testsuites->firstChild;
-// $testsuiteUnit = $testsuiteTotal->firstChild;
-// $testsuitesSearched = $testsuiteUnit->childNodes;
-echo 'Length: ' . $testsuiteTotal->childNodes->length . PHP_EOL;
-echo 'tag name 1: ' . $testsuiteTotal->nodeName . PHP_EOL;
-print_r($testsuiteTotal->getAttributeNames());
-$testsuiteSearched = $testsuiteTotal->firstChild;
-echo 'tag name 2: ' . $testsuiteSearched->nodeName . PHP_EOL;
-// $testsuitesSearched = $testsuiteTotal->childNodes;
+
+// Find <testsuite name="Unit"/>
+// $testsuite = $dom->getElementsByTagName('testsuite')->item(0);
+// echo 'name: ' . $testsuite->getAttribute('name') . PHP_EOL;
+$testsuite_root = array_reduce(iterator_to_array($dom->getElementsByTagName('testsuite')), function ($carry, \DOMElement $item) use ($testsuites): \DOMElement {
+    if ($item->parentNode === $testsuites) {
+        $carry = $item;
+    }
+    return $carry;
+});
+$testsuite_unit = array_reduce(iterator_to_array($dom->getElementsByTagName('testsuite')), function ($carry, \DOMElement $item): \DOMElement {
+    if ($item->getAttribute('name') === 'Unit') {
+        $carry = $item;
+    }
+
+    return $carry;
+});
+$testsuites->removeChild($testsuite_root);
+
+foreach ($testsuite_unit->childNodes as $child) {
+    echo 'tag name: ' . $child->nodeName . PHP_EOL;
+    $testsuites->appendChild($child);
+}
+
+
+// /** @var DOMElement $testsuiteTotal */
+// $testsuiteTotal = $testsuites->firstChild;
+// // $testsuiteUnit = $testsuiteTotal->firstChild;
+// // $testsuitesSearched = $testsuiteUnit->childNodes;
+// echo 'Length 0: ' . $testsuites->childNodes->length . PHP_EOL;
+// echo 'Length: ' . $testsuiteTotal->childNodes->length . PHP_EOL;
+// echo 'tag name 1: ' . $testsuiteTotal->nodeName . PHP_EOL;
+// print_r($testsuiteTotal->getAttributeNames());
+// $testsuiteSearched = $testsuiteTotal->firstChild;
+// echo 'tag name 2: ' . $testsuiteSearched->nodeName . PHP_EOL;
+// // $testsuitesSearched = $testsuiteTotal->childNodes;
 
 
 // $testsuites->replaceChildren($testsuitesSearched);
