9a678b9fc6fb7269fce5ef4c2ffe40845cac9b51
1stthomas <thomas.pluess@students.ffhs.ch>
2025-08-27 18:39:52 +0200

    #27 fix sequence

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 // ini_set('include_path', ini_get('include_path'));
 
 $loader = require __DIR__ . '/../vendor/autoload.php';
